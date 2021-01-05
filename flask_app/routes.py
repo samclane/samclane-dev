3aa7c7dc486dae34f9884e68a774580a7cafe60b
samclane <samclane@gmail.com>
2021-01-05 16:41:02 -0700

    Fixed unsolved reference to 'abort'

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, abort
 from flask_login import login_user, logout_user, login_required, current_user
 from passlib.hash import sha256_crypt
 
